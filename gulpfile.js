ac905ae0a6d764d3244d777ab223ad2ae02c79f8
André Thériault <atheriault2@coveo.com>
2016-09-15 11:32:03 -0400

    Removed useless gulp step

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var SALESFORCE_CONFIG = require('./salesforce.config.json');
 gulp.task('default', function () {
   return gulp.src(['./pkg/**'], { base: "." })
     .pipe(zip('pkg.zip'))
-    .pipe(gulp.dest('dist/'))
     .pipe(forceDeploy({
       username: SALESFORCE_CONFIG.SF_USERNAME,
       password: SALESFORCE_CONFIG.SF_PASSWORD + SALESFORCE_CONFIG.SF_SECURITYTOKEN,
